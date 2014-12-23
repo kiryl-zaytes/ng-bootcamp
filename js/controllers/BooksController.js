92d52d98e12961721b7ced5fcc143fbce2081d95
Juriy Bura <juriy.bura@gmail.com>
2014-12-23 21:28:49 +0800

    Added description

--- js/controllers/BooksController.js
+++ js/controllers/BooksController.js
@@ -2,4 +2,13 @@ var module = angular.module('bookstore');
 
 module.controller('BooksController', function($scope) {
 	$scope.books = data;
+
+	$scope.limitBy = function(text, maxLen) {
+		var cut = text.substr(0, maxLen);
+		return cut.substr(0, Math.min(cut.length, cut.lastIndexOf(' ')));
+	}
+
+	$scope.trimHtml = function(html) {
+		return html ? html.replace(/<(?:.|\n)*?>/gm, '') : '';
+	}
 });
